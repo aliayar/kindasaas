3338201975fd9d16cc5eb3594b097c1e6a518e0a
aliayar <alaettinayar@gmail.com>
2021-01-25 16:58:29 +0100

    setup tests added basic tests for static pages

---
+++ snakeeyes/tests/conftest.py
@@ -0,0 +1,36 @@
+import pytest
+
+from snakeeyes.app import create_app
+
+
+@pytest.fixture(scope='session')
+def app():
+    """
+    Setup flask test app, executed only once.
+    :return: Flask app
+    """
+    params = {
+        'DEBUG': False,
+        'TESTING': True
+    }
+
+    _app = create_app(settings_override=params)
+
+    # Establish an application context before running the tests.
+
+    ctx = _app.app_context()
+    ctx.push()
+
+    yield _app
+
+    ctx.pop()
+
+
+@pytest.fixture(scope='function')
+def client(app):
+    """
+    Setup an app client. executed for each test function
+    :param app: PyTest fixture
+    :return: Flask app client
+    """
+    yield app.test_client()
